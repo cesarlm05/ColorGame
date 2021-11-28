352657b2f4634d6b8a0e87c0359d0df5f893f9ac
César L. Medina <cesarlm05@gmail.com>
2021-11-28 20:12:35 -0300

    Cambios en Levels

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,29 @@
 var colores = [];
 var pickedColor;
 var posicion;
-var displayColor = document.querySelector("#colorDisplay");
+var displayColor = document.querySelector("#colorDisplay");//Mensaje.
 var squares = document.querySelectorAll(".square");
-var btnNevoColor = document.querySelector("#nuevo");
+var btnReset = document.querySelector("#reset");
+var btnFacil = document.querySelector("#facil");
+var btnMedio = document.querySelector("#medio");
+var btnDificil = document.querySelector("#dificil");
+
+var iten;
 
 load();
 
 function load(){
-  colorRandon();
+  colorRandon(iten);
   colorSquares(); 
   display();
   reset();
 };
 
-btnNevoColor.addEventListener("click", () => load()); //Funcion arrow
+btnReset.addEventListener("click", () => load()); //Funcion arrow
+
+//btnFacil.addEventListener("click", iten = 3, () => load());
+//btnMedio.addEventListener("click", iten = 6, () => load());
+btnDificil.addEventListener("click", iten = 12, () => load());
 
 function generarColor() {
   var r = Math.floor(Math.random() * 255);
@@ -23,8 +32,8 @@ function generarColor() {
   return "rgb(" + r + ", " + g + ", " + b + ")";
 };
 
-function colorRandon() {
-  for (let i = 0; i < 6; i++) {
+function colorRandon(itens) {
+  for (let i = 0; i < itens; i++) {
       let color = generarColor();
       colores.push(color);
   };
@@ -54,6 +63,8 @@ function comparar() {
         colorGanador();
       } else {
         this.style.background = "#333";
+        displayColor.innerHTML = '¡¡¡La cagaste!!!';
+        squares[i].style.display="none"
       };
     });
   };
@@ -65,6 +76,23 @@ function colorGanador() {
   };
 };
 
-function reset() {
-  colores = [];  
-};
+/*function reset() {
+  colores = [];
+};*/
+
+function reset(){
+  colores= [];
+  pickedColor=generarColor()
+  displayColor.textContent=pickedColor
+  for (var i = 0; i < squares.length; i++) {
+    if(colores[i]){
+    squares[i].style.background=colores[i]
+    squares[i].style.display="none"
+  }else{
+    squares[i].style.display="block"
+  }
+  }
+  //h1.style.background="steelblue"
+  //messageDisplay.textContent=""
+  //btnReset.textContent="New Colors"
+}
